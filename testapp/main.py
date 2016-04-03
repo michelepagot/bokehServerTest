97a9f187f5c01aa371d72d5d2e5dc71b6c608f6e
michelepagot <michele.pagot@hotmail.it>
2016-04-03 03:17:00 +0200

    Some more experiment with DropDown callback

--- testapp/main.py
+++ testapp/main.py
@@ -1,37 +1,47 @@
 import numpy as np
 
-from bokeh.models import Button
-from bokeh.palettes import RdYlBu3
+from bokeh.models import Button, Dropdown
 from bokeh.plotting import figure, curdoc, vplot
+
 
 # create a plot and style its properties
 p = figure(x_range=(0, 100), y_range=(0, 100), toolbar_location=None)
-p.border_fill_color = 'blue'
+p.border_fill_color = 'grey'
 p.background_fill_color = 'white'
 p.outline_line_color = None
 p.grid.grid_line_color = None
+
+i = 0
+circle_color = "olive"
 
 # add a text renderer to out plot (no data yet)
 r = p.text(x=[], y=[], text=[], text_color=[], text_font_size="20pt",
            text_baseline="middle", text_align="center")
 
-i = 0
-
 ds = r.data_source
 
 # create a callback that will add a number in a random location
-def callback():
+def callbackButton():
     global i
-    ds.data['x'].append(np.random.random()*70 + 15)
-    ds.data['y'].append(np.random.random()*70 + 15)
-    ds.data['text_color'].append(RdYlBu3[i%3])
+    global circle_color
+    ds.data['x'].append(np.random.random() * 70 + 15)
+    ds.data['y'].append(np.random.random() * 70 + 15)
+    ds.data['text_color'].append(circle_color)
     ds.data['text'].append(str(i))
     ds.trigger('data', ds.data, ds.data)
     i = i + 1
 
+def callbackSelect(color_in):
+    global circle_color
+    circle_color = color_in
+
+
 # add a button widget and configure with the call back
 button = Button(label="Add Random Data")
-button.on_click(callback)
+select = Dropdown(menu=[('Black','black'),('Green','green'),('Blue','blue'),None,('Red','red')],default_value='red')
+
+button.on_click(callbackButton)
+select.on_click(callbackSelect)
 
 # put the button and plot in a layout and add to the document
-curdoc().add_root(vplot(button, p))
+curdoc().add_root(vplot(button,select, p))
